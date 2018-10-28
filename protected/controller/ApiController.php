d01787dc2c832aff5c7417f5ab5c753faa4ae153
ZsgsDesign <zsgsdesign@gmail.com>
2018-10-28 18:20:35 +0800

    push to deploy

--- protected/controller/ApiController.php
+++ protected/controller/ApiController.php
@@ -5,6 +5,6 @@ class ApiController extends BaseController
     public function actionTest()
     {
         $submit_time=date("Y-m-d H:i:s");
-        SUCCESS::Catcher("成功！", array("time"=>$submit_time));
+        SUCCESS::Catcher("成功！!", array("time"=>$submit_time));
     }
 }
