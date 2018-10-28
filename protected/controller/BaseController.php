93b9eb6f48acb98e6ded59c312031a5e96d36e03
ZsgsDesign <zsgsdesign@gmail.com>
2018-10-28 16:52:25 +0800

    update version

--- protected/controller/BaseController.php
+++ protected/controller/BaseController.php
@@ -9,8 +9,8 @@ class BaseController extends Controller
             "author"=>"John Zhang",
             "organization"=>"SAST of NJUPT",
             "developer"=>"John Zhang",
-            "version"=>"0.2.2=3 Beta",
-            "subversion"=>"201810271708",
+            "version"=>"0.3.0 Beta",
+            "subversion"=>"201810281706",
         );
         $this->title="";
         // $this->bg="https://1cf.co/searchEngine/img/bg.jpg";
