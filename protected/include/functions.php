6de0a39e5d48c1e783b0f1b65f6d40019e8cc1aa
ZsgsDesign <zsgsdesign@gmail.com>
2018-10-29 13:11:30 +0800

    remove cognitive services support

--- protected/include/functions.php
+++ protected/include/functions.php
@@ -120,31 +120,6 @@ function getusersettings($loginid)
     return $result;
 }
 
-function get_thumbnail($data_string)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_URL, 'https://api.projectoxford.ai/vision/v1.0/generateThumbnail?width=100&height=100&smartCropping=true)');
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        array(
-        'Content-Type: application/json; charset=utf-8',
-        'Host: api.projectoxford.ai',
-        'Ocp-Apim-Subscription-Key: 86c2a4018d964a64b43558ed925eaea4',
-        'Content-Length: ' . strlen($data_string))
-    );
-            
-    ob_start();
-    curl_exec($ch);
-    $return_content = ob_get_contents();
-    ob_end_clean();
-      
-    $return_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    return array($return_code, $return_content);
-}
-
 function sizeConverter($bit)
 {
     $type = array('Bytes','KB','MB','GB','TB');
