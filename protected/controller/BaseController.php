a11346f9c47fb323a055cbcd852918cbca8ee406
ZsgsDesign <zsgsdesign@gmail.com>
2018-11-04 21:40:54 +0800

    rgws

--- protected/controller/BaseController.php
+++ protected/controller/BaseController.php
@@ -5,12 +5,13 @@ class BaseController extends Controller
     public $layout = "layout.html";
     public function init()
     {
+        echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
         $this->version_info=array(
             "author"=>"John Zhang",
             "organization"=>"SAST of NJUPT",
             "developer"=>"John Zhang",
             "version"=>"0.6.2 Beta",
-            "subversion"=>"20181104210956",
+            "subversion"=>"20181104214050",
         );
         $this->title="";
         // $this->bg="https://1cf.co/searchEngine/img/bg.jpg";
