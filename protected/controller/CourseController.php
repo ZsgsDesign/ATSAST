33594d9b187dc927674cd6863af2d6b6b29ab8e8
ZsgsDesign <zsgsdesign@gmail.com>
2018-11-04 16:41:24 +0800

    test2

--- protected/controller/CourseController.php
+++ protected/controller/CourseController.php
@@ -336,7 +336,8 @@ class CourseController extends BaseController
                     return $this->jump("/courses");
                 }
                 if($syllabus_info["signed"]==0){
-                    return $this->jump("/course/$cid/detail2");
+                    var_dump($syllabus_info);
+                    return $this->jump("/course/$cid/detail");
                 }
                 $sign_status=$sign->find(array("cid=:cid and syid=:syid and uid=:uid",":cid"=>$cid,":syid"=>$syid,":uid"=>$this->userinfo['uid']));
                 if (empty($sign_status)) {
