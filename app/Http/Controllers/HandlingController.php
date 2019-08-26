bf3c01580dfd8d0bc6e39808b7936507179ed990
Gou Faan <goufaan@gmail.com>
2019-08-26 20:16:02 +0800

    rename controller

--- app/Http/Controllers/HandlingController.php
+++ app/Http/Controllers/HandlingController.php
@@ -7,22 +7,22 @@ use Illuminate\Http\Request;
 class MaterialHandlingSystemController extends Controller
 {
     public function index(){
-
+        return view('handilng.index');
     }
 
     public function publish(){
-
+        return view('handilng.publish');
     }
 
     public function cart(){
-
+        return view('handilng.cart');
     }
 
     public function detail($itemId){
-
+        return view('handilng.detail');
     }
 
     public function order($orderId){
-
+        return view('handilng.order');
     }
 }
