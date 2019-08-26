dd2112c1ca298af95c962be57ee449c7f780d3b4
Gou Faan <goufaan@gmail.com>
2019-08-26 21:36:51 +0800

    add detail

--- app/Models/ItemModel.php
+++ app/Models/ItemModel.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Facades\DB;
 
 class ItemModel extends Model
 {
-    protected $table='item';
+    protected $table = 'item';
+    const CREATED_AT = 'create_time';
+    protected $primaryKey = 'iid';
 
     public function getItems(){
         return DB::table($this->table)->orderBy('create_time', 'desc')->paginate(12);
