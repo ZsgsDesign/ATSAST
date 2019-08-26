84a26896107004dd86c54f0720d0132c96802552
Gou Faan <goufaan@gmail.com>
2019-08-26 20:43:52 +0800

    fix controller

--- app/Http/Controllers/HandlingController.php
+++ app/Http/Controllers/HandlingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MaterialHandlingSystemController extends Controller
+class HandlingController extends Controller
 {
     public function index(){
         return view('handilng.index');
