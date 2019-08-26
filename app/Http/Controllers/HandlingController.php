3812080f874aefd42f70f3ac02a3660e3e9aaea9
Gou Faan <goufaan@gmail.com>
2019-08-26 20:47:11 +0800

    basic view accessor

--- app/Http/Controllers/HandlingController.php
+++ app/Http/Controllers/HandlingController.php
@@ -7,22 +7,42 @@ use Illuminate\Http\Request;
 class HandlingController extends Controller
 {
     public function index(){
-        return view('handilng.index');
+        return view('handling.index',[
+            'page_title'=>"借还",
+            'site_title'=>"SAST教学辅助平台",
+            'navigation'=>"Handling"
+        ]);
     }
 
     public function publish(){
-        return view('handilng.publish');
+        return view('handling.publish',[
+            'page_title'=>"发布物品",
+            'site_title'=>"SAST教学辅助平台",
+            'navigation'=>"Handling"
+        ]);
     }
 
     public function cart(){
-        return view('handilng.cart');
+        return view('handling.cart',[
+            'page_title'=>"购物车",
+            'site_title'=>"SAST教学辅助平台",
+            'navigation'=>"Handling"
+        ]);
     }
 
     public function detail($itemId){
-        return view('handilng.detail');
+        return view('handling.detail',[
+            'page_title'=>"物品详情",
+            'site_title'=>"SAST教学辅助平台",
+            'navigation'=>"Handling"
+        ]);
     }
 
     public function order($orderId){
-        return view('handilng.order');
+        return view('handling.order',[
+            'page_title'=>"我的订单",
+            'site_title'=>"SAST教学辅助平台",
+            'navigation'=>"Handling"
+        ]);
     }
 }
