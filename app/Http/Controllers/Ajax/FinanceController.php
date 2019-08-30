315bfc37c3016fb67a1f645b68b7636571f8777d
X3ZvaWQ <x3zvawq@gmail.com>
2019-08-30 10:38:30 +0800

    init: finance modules

---
+++ app/Http/Controllers/Ajax/FinanceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Ajax;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FinanceController extends Controller
+{
+    public function create(Request $request)
+    {
+
+    }
+}
---
+++ app/Http/Controllers/FinanceController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FinanceController extends Controller
+{
+    public function home()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function view()
+    {
+
+    }
+
+    public function history()
+    {
+
+    }
+
+
+}
