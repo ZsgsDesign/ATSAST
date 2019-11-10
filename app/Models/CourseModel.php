189c943c38bbcc9fa3d6412dda653d71bda9f7bc
X3ZvaWQ <x3zvawq@gmail.com>
2019-11-10 13:07:53 +0800

    fix: course order by id desc

--- app/Models/CourseModel.php
+++ app/Models/CourseModel.php
@@ -13,7 +13,7 @@ class CourseModel extends Model
 {
     public function list()
     {
-        $paginator = DB::table('courses')->orderBy('cid', 'desc')->paginate(9);
+        $paginator = DB::table('courses')->orderBy('cid', 'asc')->paginate(9);
         $list = $paginator->all();
         return [
             'paginator' => $paginator,
