4b945f86e0b914759618517abaf6ce3d2fb97329
X3ZvaWQ <x3zvawq@gmail.com>
2019-11-11 11:17:30 +0800

    hotfix: User fillable attr without SID

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'avatar', 'album', 'cloud_size', 'cur_version',
+        'name', 'email', 'password', 'avatar', 'album', 'cloud_size', 'cur_version', 'SID',
     ];
 
     /**
